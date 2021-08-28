89b01597b393d5fe17fbce5b64e3295b5fe500fb
Roman Panchenko <roman.p@aquimarketing.com>
2021-08-28 18:26:32 +0300

    removed console.log

--- fakeDB-service/handlers/fakeDB.js
+++ fakeDB-service/handlers/fakeDB.js
@@ -1,7 +1,6 @@
 'use strict';
 const allProducts = require('./fakeDB.json');
 module.exports.getAllProducts = async (event) => {
-  console.log('event', event);
   return {
     statusCode: 200,
     body: JSON.stringify(allProducts)
