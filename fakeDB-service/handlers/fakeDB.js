b35ce7b008bca8211ae1678f1780fdb31222acdb
Roman Panchenko <roman.p@aquimarketing.com>
2021-09-08 19:15:32 +0300

    task-4 db connection

--- fakeDB-service/handlers/fakeDB.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const allProducts = require('./fakeDB.json');
-module.exports.getAllProducts = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(allProducts)
-  };
-};
--- product-service/handlers/getProducts.js
+++ product-service/handlers/getProducts.js
@@ -1,12 +1,26 @@
 'use strict';
-
-const fetchProducts = require('../helpers/fetchProducts');
+const { Client } = require('pg');
+// const fetchProducts = require('../helpers/fetchProducts');
 const { headers } = require('../config.json');
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+const dbOptions = {
+  host:PG_HOST,
+  port: PG_PORT,
+  database: PG_DATABASE,
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  connectionTimeoutMillis: 5000
+};
 
 module.exports.getProducts = async (event) => {
+  const client = new Client(dbOptions);
+  await client.connect();
   let allProducts;
   try {
-    allProducts = await fetchProducts();
+    allProducts = await client.query('SELECT id, title, description, price, count FROM products JOIN stock ON id = product_id');
   } catch (e) {
     return {
       statusCode: e.code,
@@ -18,6 +32,6 @@ module.exports.getProducts = async (event) => {
   return {
     statusCode: 200,
     headers,
-    body: JSON.stringify(allProducts)
+    body: JSON.stringify(allProducts.rows)
   };
 };
