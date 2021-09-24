b9ea78b1ca5ea08da667691c1fae535202b9b121
Roman Panchenko <roman.p@aquimarketing.com>
2021-09-25 01:35:51 +0300

    feat: parser

--- import-service/handlers/importFileParser.js
+++ import-service/handlers/importFileParser.js
@@ -1,30 +1,31 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3({region: 'eu-west-1'});
+const CSV = require('csv-parser');
+const Bucket = 'vedro-for-import-practice';
 
 module.exports.fileParser = async (event) => {
-    console.log(event);
-
+    console.log('EventInfo:', event);
+    const Key = event.Records.s3.object.key;
+    console.log('Key:', event);
     const params = {
-        Bucket: 'vedro-for-import-practice',
-        Key: ''
+        Bucket,
+        Key: Key
     };
+    console.log('Params:', params);
+    await new Promise((resolve, reject) => {
+        const s3Stream = s3.getObject(params).createReadStream();
+        s3Stream
+            .on('data', (chunk) => {
+                console.log(chunk);
+            })
+            .on('error', (error) => {
+                reject('error');
+                throw error;
+            })
+            .on('end', () => {
+                console.log('END')
+            });
+    });
 
-    const s3Stream = s3.getObject(params).createReadStream();
-    s3Stream
-        .on('data', (data) => {})
-        .on('error', (error) => {})
-        .on('end', () => {});
-
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: 'The file successfully uploaded!',
-            },
-            null,
-            2
-        ),
-    };
 
 };
